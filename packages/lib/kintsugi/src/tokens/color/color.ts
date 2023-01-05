960fa1301dd413e0d64ec5e89869f66c7b839ebe
Monica <101018690+monicadomigop@users.noreply.github.com>
2023-01-06 00:13:46 +0100

    Token color

---
+++ packages/lib/kintsugi/src/tokens/color/color.ts
@@ -0,0 +1,25 @@
+export const color = {
+  primary: {
+    default: '#00509D',
+    active: '#003A71',
+    disabled: '#0486FF',
+  },
+  secondary: {
+    default: '#D19E00',
+    active: '#957000',
+    disabled: '#FFC928',
+  },
+  foundation: {
+    primary: '#140C00',
+    secondary: '#504538',
+    tertiary: '#FFC928',
+  },
+  background: {
+    primary: '#FFFFFF',
+    secondary: '#F6F6F6',
+  },
+  system: {
+    positive: '#05944F',
+    negative: '#E11900',
+  },
+}
