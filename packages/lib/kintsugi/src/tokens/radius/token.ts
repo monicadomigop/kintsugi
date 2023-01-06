4489c6af26ec763231e822d5d577437e8ab27316
Monica <101018690+monicadomigop@users.noreply.github.com>
2023-01-06 01:17:47 +0100

    Create token radius

---
+++ packages/lib/kintsugi/src/tokens/radius/token.ts
@@ -0,0 +1,19 @@
+export interface TokenRadiusProps {
+  XS: number
+  S: number
+  M: number
+  L: number
+  XL: number
+  XXL: number
+  XXXL: number
+}
+
+export const radius: TokenRadiusProps = {
+  XS: 2,
+  S: 4,
+  M: 8,
+  L: 12,
+  XL: 24,
+  XXL: 32,
+  XXXL: 100,
+}
