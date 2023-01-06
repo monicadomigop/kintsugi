df760e781f5bfdd283847485ca6081c09a74ebdc
Monica <101018690+monicadomigop@users.noreply.github.com>
2023-01-06 17:21:02 +0100

    update jest setup

--- packages/lib/kintsugi/jest.config.js
+++ packages/lib/kintsugi/jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   displayName: 'kintsugi',
-  setupFilesAfterEnv: ['./jest-setup.setup.ts'],
   preset: '../../../jest.preset.js',
   transform: {
     '^.+\\.[tj]sx?$': 'babel-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../../coverage/packages/lib/kintsugi',
-}
+  setupFilesAfterEnv: ['./jest.setup.ts'],
+};
